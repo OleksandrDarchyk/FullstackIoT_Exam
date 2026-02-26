819834246a664cd72a5746d31acb90d38cecf64f
OleksandrDarchyk <oledar01@easv365.dk>
2026-02-26 13:38:35 +0100

    add react base url

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const local = "http://localhost:5233";
+const prod = "https://API_DOMAIN"; // TODO: Replace with the Fly.io API base URL once deployed.
+
+export const BASE_URL = import.meta.env.PROD ? prod : local;
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
