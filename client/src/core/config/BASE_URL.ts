192062c8e8adb333c79939936e9c93bbb80f14e1
OleksandrDarchyk <oledar01@easv365.dk>
2026-03-01 17:17:26 +0100

    deploy

--- client/src/core/config/BASE_URL.ts
+++ client/src/core/config/BASE_URL.ts
@@ -1,5 +1,5 @@
 const local = "http://localhost:5039";
-const prod = "https://MY-IOT-API.fly.dev";
+const prod = "https://server-windfarm-hill-1973.fly.dev";
 
 export const BASE_URL = import.meta.env.PROD ? prod : local;
 export const SSE_URL = `${BASE_URL}/sse`;
